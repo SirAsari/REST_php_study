43550d030dcc97cdbce04f44abd6cd198e24676e
asari <muhamadasari@smkwikrama.sch.id>
2023-12-24 01:59:20 +0700

    Adding both folders of api resting

---
+++ php_rest_mine/api/post/read.php
@@ -0,0 +1,14 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+
+include_once '../../config/Database.php';
+include_once '../../models/Post.php';
+
+$database = new Database();
+$db = $database->connect();
+
+$post = new Post($db);
+$result = $post->read();
+$num = $result->rowCount();
+
